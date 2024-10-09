174a95cbf0999d4bdb66037f3fe3953403539465
JuliaGabP <juliagabpaulino@outlook.com>
2024-10-09 13:39:08 -0300

    Inserção do contator de visitas

--- meu_portifolio/app.py
+++ meu_portifolio/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, session
+from flask_session import Session
 
 app = Flask(__name__)
+
+# Configurar a sessão do Flask para armazenar dados no lado do servidor
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 # Rota para a página inicial
 @app.route('/')
 def index():
-    return render_template('index.html')
+# Verificar se já existe um contador de visitas
+    if 'visits' not in session:
+        session['visits'] = 0
+    # Incrementar o contador de visitas
+    session['visits'] = session['visits'] + 1
+    
+    # Renderizar a página principal e passar o contador de visitas
+    return render_template('index.html', visits=session['visits'])
 
 # Rota para a página de trabalhos
 @app.route('/works')
